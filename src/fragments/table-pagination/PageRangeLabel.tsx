50ecc88d5692f4a70764d38c79cb7a781e5bd556
Rob Doneux <robertdoneux@gmail.com>
2026-03-01 19:56:20 +0000

    refactor: optimize label calculation in PageRangeLabel component using useMemo

--- src/fragments/table-pagination/PageRangeLabel.tsx
+++ src/fragments/table-pagination/PageRangeLabel.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/joy'
 import type { Pagination } from './pagination-types'
+import { useMemo } from 'react'
 
 interface PageRangeLabelProps {
   resultsLength: number
@@ -9,17 +10,17 @@ export default function PageRangeLabel({
   resultsLength,
   pagination,
 }: PageRangeLabelProps) {
-  const getLabelDisplayedRowsTo = () => {
+  const labelDisplayedRowsTo = useMemo(() => {
     if (resultsLength === 0) {
       return (pagination.page + 1) * pagination.size
     }
     return Math.min(pagination.total, (pagination.page + 1) * pagination.size)
-  }
+  }, [resultsLength, pagination])
 
   return (
     <Typography fontSize={12} lineHeight={3}>
       {resultsLength === 0 ? 0 : pagination.page * pagination.size + 1} -{' '}
-      {getLabelDisplayedRowsTo()} of {pagination.total}
+      {labelDisplayedRowsTo} of {pagination.total}
     </Typography>
   )
 }
